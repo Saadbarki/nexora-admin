a88a4cf91725d7380a7f90894d659dfa4ac8af8a
Saad Barki <your-github-saad.khan0304@gmail.com>
2026-03-02 15:40:49 +0500

    fix: linux docker setup, nginx proxy config, jwt secret

--- frontend/src/config/serverApiConfig.js
+++ frontend/src/config/serverApiConfig.js
@@ -1,13 +1,9 @@
-export const API_BASE_URL = 'http://localhost:8888/api/';
-export const BASE_URL = 'http://localhost:8888/';
-
+export const API_BASE_URL = '/api/';
+export const BASE_URL = '/';
 export const WEBSITE_URL =
   import.meta.env.PROD
     ? 'http://cloud.nexoraapp.com/'
     : 'http://localhost:3000/';
-
-export const DOWNLOAD_BASE_URL = 'http://localhost:8888/download/';
-
+export const DOWNLOAD_BASE_URL = '/download/';
 export const ACCESS_TOKEN_NAME = 'x-auth-token';
-
 export const FILE_BASE_URL = import.meta.env.VITE_FILE_BASE_URL;
